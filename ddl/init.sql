494490b492c23a64bdf27fe3641096c471ba3370
Dan Galdi <dmgaldi@upenn.edu>
2023-02-28 14:44:03 -0500

    Update ddl

--- ddl/init.sql
+++ ddl/init.sql
@@ -1,32 +1,42 @@
-CREATE TABLE usagemetrics.analysis_histogram (
+CREATE TABLE usagemetrics.job_runs (
     run_id                    VARCHAR(20) NOT NULL,
-    count_bucket              VARCHAR(20) NOT NULL,
-    registered_users_analyses NUMBER,
-    guests_analyses           NUMBER,
-    registered_users_filters  NUMBER,
-    guests_filters            NUMBER,
-    PRIMARY KEY(id, count_bucket)
-);
+    start_month               DATE NOT NULL,
+    active                    NUMBER(1,0) NOT NULL,
+    raw_analysis_data         CLOB NOT NULL
+)
 
-CREATE TABLE usagemetrics.raw_analysis_data (
-    run_id                  VARCHAR(20) NOT NULL,
-    raw_data                CLOB,
-    PRIMARY KEY(id)
+CREATE TABLE usagemetrics.analysis_histogram (
+    run_id                    VARCHAR(20) NOT NULL REFERENCES usagemetrics.job_runs,
+    count_bucket              VARCHAR(20) NOT NULL,
+    registered_users_analyses NUMBER NOT NULL,
+    guests_analyses           NUMBER NOT NULL,
+    registered_users_filters  NUMBER NOT NULL,
+    guests_filters            NUMBER NOT NULL,
+    PRIMARY KEY(run_id, count_bucket),
+
+    FOREIGN KEY (run_id)
+        REFERENCES usagemetrics.job_runs(run_id)
 );
 
 CREATE TABLE usagemetrics.downloads_histogram (
     run_id                        VARCHAR(20) NOT NULL,
     study_id                      VARCHAR(20) NOT NULL,
-    num_users_full_download       NUMBER,
-    num_users_subset_download     NUMBER,
+    num_users_full_download       NUMBER NOT NULL,
+    num_users_subset_download     NUMBER NOT NULL,
     PRIMARY KEY(id, study_id)
+
+    FOREIGN KEY (run_id)
+        REFERENCES usagemetrics.job_runs(run_id)
 );
 
 CREATE TABLE usagemetrics.aggregate_user_stats (
     run_id                    VARCHAR(20) NOT NULL,
-    count                     NUMBER,
-    num_filters               NUMBER,
-    num_visualizations        NUMBER,
-    user_category             ENUM('guest', 'registered'),
+    count                     NUMBER NOT NULL,
+    num_filters               NUMBER NOT NULL,
+    num_visualizations        NUMBER NOT NULL,
+    user_category             ENUM('guest', 'registered') NOT NULL,
     PRIMARY KEY(id, study_id)
+
+    FOREIGN KEY (run_id)
+        REFERENCES usagemetrics.job_runs(run_id)
 );
